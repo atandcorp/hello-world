7537e2c763f68d2493c6cbdda3d978f1552b3543
balogunrash <63379157+balogunrash@users.noreply.github.com>
2021-03-25 10:27:06 -0500

    Update hello-world.tf

--- deployments/terraform/hello-world.tf
+++ deployments/terraform/hello-world.tf
@@ -1,4 +1,4 @@
-module "helm_deploy" {
+module "hello_deploy" {
   source                    = "fuchicorp/chart/helm"
   deployment_name           = "${var.deployment_name}"
   deployment_environment    = "${var.deployment_environment}"
